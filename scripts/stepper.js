7547eec7bbfc4d31ead1254f396017f47953bc6b
Rogok-Serg <rogoksv@gmail.com>
2025-05-08 14:24:39 +0300

    add section: step-progress and selected-car

--- scripts/stepper.js
+++ scripts/stepper.js
@@ -3,19 +3,30 @@ const isMobileOrTablet = () => window.innerWidth <= 1199;
 if (isMobileOrTablet) {
   function updateStep(newStep) {
     const steps = document.querySelectorAll(".step");
+    const stepWrappers = document.querySelectorAll(".step-wrapper");
     const lines = document.querySelectorAll(".progress-line");
 
     steps.forEach((step, index) => {
       step.classList.toggle("active", index <= newStep);
     });
 
-    lines.forEach((line, index) => {
-      line.style.backgroundColor = index < newStep ? "#B68867" : "#444";
+    stepWrappers.forEach((wrapper, index) => {
+      const leftLine = wrapper.querySelector(".progress-line.left");
+      const rightLine = wrapper.querySelector(".progress-line.right");
+
+      if (rightLine) {
+        rightLine.style.backgroundColor = index <= newStep ? "#B68867" : "#444";
+      }
+      if (leftLine) {
+        leftLine.style.backgroundColor = index <= newStep ? "#B68867" : "#444";
+      }
     });
 
     currentStep = newStep;
   }
+
   const stepBar = document.querySelector(".step-progress");
+  const selectedCar = document.querySelector(".selected-car");
   const btnProceed = document.querySelector(".btn-proceed");
   const btnExtras = document.querySelector(".btn-extras");
   const btnInsurance = document.querySelector(".btn-insurance");
@@ -23,6 +34,7 @@ if (isMobileOrTablet) {
 
   btnProceed.addEventListener("click", () => {
     stepBar.style.display = "flex";
+    selectedCar.style.display = "flex";
     updateStep(0);
   });
 
